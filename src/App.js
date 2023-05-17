40adb1810190eacc7cc836e721e3691de03a59b6
Albertoho09 <albertoraikeor@gmail.com>
2023-05-18 01:20:35 +0200

    ofu hijo 	new file:   .vscode/launch.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/logo/perroElfary.jpeg 	modified:   src/service/usuarioService.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 
 import './App.css';
 import { usuarioService } from './service/usuarioService.js';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import { Dialog } from 'primereact/dialog';
+import { Divider } from 'primereact/divider';
+import { Toast } from 'primereact/toast';
+import { Password } from 'primereact/password';
+import { Calendar } from 'primereact/calendar';
+import { Avatar } from 'primereact/avatar';
 
 import "primereact/resources/themes/mdc-dark-deeppurple/theme.css";
 import "primereact/resources/primereact.min.css";
 import 'primeicons/primeicons.css';
 import "primeflex/primeflex.css";
-import "./logo/captura.png";
 import imagenInicio from "./logo/captura.png";
 import imagenInicioSesion from "./logo/logocompleto.png"
 
@@ -20,6 +24,20 @@ export default class App extends Component {
 
   constructor() {
     super();
+    this.toast = createRef();
+    
+    this.personaregistro = {
+      codigo: null,
+      nick: '',
+      correo: '',
+      contrasenia: '',
+      nombre: '',
+      apellido: '',
+      fechanac: '',
+      namigos: 0,
+      fotoperfil: null,
+      npublicaciones: 0
+    }
 
     this.state = {
       displayBasic: false,
@@ -32,15 +50,26 @@ export default class App extends Component {
 
       codigosesion: null,
 
-      inputValue1: '',
-      inputValue2: ''
+      //Variables Inicio sesión
+      correoInicio: '',
+      contraseniaInicio: '',
+
+      //Variables Registro
+      nick: '',
+      correoRe: '',
+      contraseniaRe: '',
+      nombreRe: '',
+      apellidoRe: '',
+      fechanacRe: '',
+
+      fotoper: null
     };
 
 
+    this.file = null;
     this.usuarioService = new usuarioService();
     this.onClick = this.onClick.bind(this);
     this.onHide = this.onHide.bind(this);
-
   }
 
   componentDidMount() {
@@ -49,17 +78,52 @@ export default class App extends Component {
 
 
   handleClick = () => {
-    console.log('Input 1:', this.state.inputValue1);
-    console.log('Input 2:', this.state.inputValue2);
-    this.usuarioService.login(this.state.inputValue1, this.state.inputValue2).then(data => this.setState({ codigosesion: data }))
+    if (this.state.correoInicio.length > 0 || this.state.contraseniaInicio > 0) {
+      this.usuarioService.login(this.state.correoInicio, this.state.contraseniaInicio).then(data => {
+        this.setState({ codigosesion: data });
+        if (data === -1) {
+          this.showError();
+        }
+      });
+    } else {
+      this.showError();
+    }
   }
 
-  handleInputChange1 = (event) => {
-    this.setState({ inputValue1: event.target.value });
+  showError = () => {
+    this.toast.current.show({ severity: 'error', summary: 'Error', detail: 'Correo o Constraseña NO VALIDO', life: 3000 });
+  }
+  showErrorRegistro = () => {
+    this.toast.current.show({ severity: 'error', summary: 'Error', detail: 'Campos NO VALIDOS o INCOMPLETOS', life: 3000 });
   }
 
-  handleInputChange2 = (event) => {
-    this.setState({ inputValue2: event.target.value });
+  correoInput = (event) => {
+    this.setState({ correoInicio: event.target.value });
+  }
+  contraseniaInput = (event) => {
+    this.setState({ contraseniaInicio: event.target.value });
+  }
+
+  correoInputRe = (event) => {
+    this.setState({ correoRe: event.target.value });
+  }
+  contraseniaInputRe = (event) => {
+    this.setState({ contraseniaRe: event.target.value });
+  }
+
+  nickInputRe = (event) => {
+    this.setState({ nick: event.target.value });
+  }
+  nombreInputRe = (event) => {
+    this.setState({ nombreRe: event.target.value });
+  }
+
+  apellidoInputRe = (event) => {
+    this.setState({ apellidoRe: event.target.value });
+  }
+
+  fechanacInputRe = (event) => {
+    this.setState({ fechanacRe: event.target.value });
   }
 
   onClick(name, position) {
@@ -83,29 +147,95 @@ export default class App extends Component {
     });
   }
 
-  renderFooter(name) {
-    return (
-      <div style={{ width: '100%' }}>
-        <div className="surface-card p-4 shadow-2 border-round w-full">
-          <div className="text-center mb-5">
-            <img src={imagenInicioSesion} alt="hyper" height={125} className="mb-3" />
-            <div className="text-900 text-3xl font-medium mb-3">Bienvenido de nuevo, Inicia Sesión:</div>
-          </div>
+  guardarfoto(e){
+    this.file = e.target.files[0];
+    console.log("file: ", this.file);
+    const url = URL.createObjectURL(this.file);
+    this.setState({
+      fotoper: url
+    });
+  }
 
-          <div>
+  comprobaciones = () =>{
+
+    if(this.state.nick.length > 0 && this.state.nombreRe.length > 0  
+      && this.state.apellidoRe.length > 0 && this.state.correoRe.length 
+      > 0 && this.state.contraseniaRe.length > 0 && this.state.fechanacRe.length > 0 && this.file != null){
+        
+        console.log("consola:", "he pasado por aqui");
+
+        this.personaregistro.apellido = this.state.apellidoRe;
+        this.personaregistro.nombre = this.state.nombreRe;
+        this.personaregistro.nick = this.state.nick;
+        this.personaregistro.fechanac = this.state.fechanacRe;
+        this.personaregistro.correo = this.state.correoRe;
+        this.personaregistro.contrasenia = this.state.contraseniaRe;
+
+        return true;
+      }
+      else{
+        return false;
+      }
+  }
+
+  registro = () =>{
+    if(this.comprobaciones()){
+      console.log("consola", "entro");
+      const formData = new FormData();
+      formData.append('file', this.file);
+      formData.append('data', JSON.stringify(this.personaregistro));
+      this.usuarioService.save(formData);
+    }else{
+      this.showErrorRegistro();
+    }
+  }
+
+  renderFooter() {
+    return (
+      <div className="card">
+        <div className="card-container yellow-container">
+          <div className="block text-center mb-5">
+            <img src={imagenInicioSesion} alt="hyper" height={125} className="mb-3" />
+            <div className="text-900 text-3xl font-medium mb-3">Bienvenido, Regístrate:</div>
+          </div>
+        </div>
+
+        <div className="flex card-container indigo-container">
+          <div className="flex-1 m-5">
+
+            <label htmlFor="nickRe" className="block text-900 font-medium mb-2">Nick</label>
+            <InputText id="input3" type="text" placeholder="Nick" className="w-full mb-3" value={this.state.nick}
+              onChange={this.nickInputRe} />
+
+            <label htmlFor="nombreRe" className="block text-900 font-medium mb-2">Nombre</label>
+            <InputText id="input4" type="text" placeholder="Nombre" className="w-full mb-3" value={this.state.nombreRe}
+              onChange={this.nombreInputRe} />
+
+            <label htmlFor="apellidoRe" className="block text-900 font-medium mb-2">Apellido/s</label>
+            <InputText id="input5" type="text" placeholder="Apellido/s" className="w-full mb-3" value={this.state.apellidoRe}
+              onChange={this.apellidoInputRe} />
+
             <label htmlFor="email" className="block text-900 font-medium mb-2">Correo Electrónico</label>
-            <InputText id="input1" type="text" placeholder="Correo Electrónico" className="w-full mb-3" value={this.state.inputValue1}
-              onChange={this.handleInputChange1} />
+            <InputText id="input6" type="text" placeholder="Correo Electrónico" className="w-full mb-3" value={this.state.correoRe}
+              onChange={this.correoInputRe} />
 
             <label htmlFor="password" className="block text-900 font-medium mb-2">Contraseña</label>
-            <InputText id="input2" type="password" placeholder="Contraseña" className="w-full mb-3" value={this.state.inputValue2}
-              onChange={this.handleInputChange2} />
+            <Password id="input7" placeholder="Contraseña" className="w-full mb-5" value={this.state.contraseniaRe} onChange={this.contraseniaInputRe} toggleMask />
+          </div>
+          <div className="flex-1 m-5">
+            <label htmlFor="password" className="block text-900 font-medium mb-2">Fecha de Nacimiento</label>
 
-            <div className="flex align-items-center justify-content-between mb-6">
-              <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot your password? po te jode</a>
+            <Calendar value={this.state.fechanacRe}  placeholder="yy-mm-dd"  className="w-full mb-5" onChange={this.fechanacInputRe} dateFormat="yy-mm-dd" />
+            <div className="flex card-container indigo-container">
+              <div className="flex-1 m-5">
+              <label htmlFor="foto" className="block text-900 font-medium mb-2">Foto Perfil</label>
+                <Avatar image={this.state.fotoper} size="xlarge" shape="circle" />
+              </div>
+              <div className="flex-1 m-5">
+                <input type='file'name='file' onChange={(e) => this.guardarfoto(e)}/>
+              </div>
             </div>
-
-            <Button label="Iniciar Sesión" icon="pi pi-user" className="w-full" onClick={this.handleClick}/>
+            <Button label="Registro" icon="pi pi-user" onClick={this.registro}/>
           </div>
         </div>
       </div>
@@ -121,11 +251,36 @@ export default class App extends Component {
             <div className="text-7xl text-primary font-bold mb-3">WebApp by Alberto Hidalgo</div>
             <p className="mt-1 mb-4 text-700 line-height-3 text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <div className="dialog-demo">
-              <div className="card">
-                <Button label="Iniciar Sesión" type="button" onClick={() => this.onClick('displayBasic')} className="mr-3 p-button-raised transition-delay-100 transition-colors transition-duration-300 bg-blue-500 hover:bg-yellow-500" />
-                <Dialog header="Inicio de Sesión" visible={this.state.displayBasic} style={{ width: '30vw' }} footer={this.renderFooter('displayBasic')} onHide={() => this.onHide('displayBasic')}>
-                </Dialog>
-                <Button label="Registrarse" type="button" className="p-button-outlined" />
+              <div className="flex flex-column md:flex-row">
+                <div className="w-full md:w-5 flex flex-column align-items-s justify-content-center gap-3 py-5">
+                  <div className="flex flex-wrap justify-content-center align-items-center gap-2">
+                    <label htmlFor="username" className="w-6rem">
+                      Correo
+                    </label>
+                    <InputText id="input1" type="text" className="w-8 mb-3" placeholder="Correo Electrónico" value={this.state.correoInicio}
+                      onChange={this.correoInput} />
+                  </div>
+                  <div className="flex flex-wrap justify-content-center align-items-center gap-2">
+                    <label htmlFor="password" className="w-6rem">
+                      Contraseña
+                    </label>
+                    <InputText id="input2" type="password" className="w-8 mb-3" placeholder="Contraseña" value={this.state.contraseniaInicio}
+                      onChange={this.contraseniaInput} />
+                  </div>
+                  <Toast ref={this.toast} position="bottom-left" />
+                  <Button label="Iniciar Sesión" icon="pi pi-user-plus" onClick={this.handleClick} className="w-10rem mx-auto"></Button>
+                </div>
+                <div className="w-full md:w-2">
+                  <Divider layout="vertical" className="hidden md:flex">
+                  </Divider>
+                  <Divider layout="horizontal" className="flex md:hidden" align="center">
+                  </Divider>
+                </div>
+                <div className="w-full md:w-5 flex align-items-center justify-content-center py-5">
+                  <Button label="Registrarse" icon="pi pi-user" onClick={() => this.onClick('displayBasic')} className="p-button-success"></Button>
+                  <Dialog header="" visible={this.state.displayBasic} style={{ width: '50vw' }} footer={this.renderFooter('displayBasic')} onHide={() => this.onHide('displayBasic')}>
+                  </Dialog>
+                </div>
               </div>
             </div>
           </section>
--- src/service/usuarioService.js
+++ src/service/usuarioService.js
@@ -3,24 +3,28 @@ import axios from 'axios';
 export class usuarioService {
     baseUrl = "http://localhost:8080/api/v1/usuario/";
 
-    getAll(){
+    getAll() {
         return axios.get(this.baseUrl + "all").then(res => res.data);
     }
 
-    save(persona) {
-        return axios.post(this.baseUrl + "save", persona).then(res => res.data);
+    save(data) {
+        axios.post(this.baseUrl + "save", data, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then(res => res.data);
     }
 
     delete(id) {
-        return axios.get(this.baseUrl + "delete/"+id).then(res => res.data);
+        return axios.get(this.baseUrl + "delete/" + id).then(res => res.data);
     }
 
-    
+
     obtener(id) {
-        return axios.get(this.baseUrl + "obtener/"+id).then(res => res.data);
+        return axios.get(this.baseUrl + "obtener/" + id).then(res => res.data);
     }
 
     login(correo, contrasenia) {
-        return axios.get(this.baseUrl + "login/"+correo+"/"+contrasenia).then(res => res.data);
+        return axios.get(this.baseUrl + "login/" + correo + "/" + contrasenia).then(res => res.data);
     }
 }
